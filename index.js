2d3c0d8305ade8ab40fc326b365dcb042dd3259d
Mollie Dickenson <molliedickenson@gmail.com>
2021-03-21 14:00:57 -0400

    commit tree eb7c67e9180b221b99b40d5f356ae08a61f0ffd1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function superbowlWin(game){
+    const winner = game.find(w => w.result ==='W')
+     if (winner === undefined){
+       return undefined
+     } else return winner.year 
+  }
+    
+  
+superbowlWin(record);
+superbowlWin(sadReality);
+
+
+
 
